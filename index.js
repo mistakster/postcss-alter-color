f95573af184de0048c25f1586fff5946fe88a061
Vladimir Kuznetsov <mistakster@gmail.com>
2018-04-09 14:18:36 +0500

    removed unused conditional check

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const postcss = require('postcss');
 const parseColor = require('parse-color');
 const cssTree = require('css-tree');
 
-function canContainColor(prop) {
-  return true;
-}
-
 function removeHashSymbol(hexValue) {
   return hexValue.substring(1);
 }
@@ -41,44 +37,42 @@ module.exports = postcss.plugin('alter-color', options => {
   return (root) => {
     root.walkRules(rule => {
       rule.walkDecls(decl => {
-        if (canContainColor(decl.prop)) {
-          const parsedValue = cssTree.parse(decl.value, {context: 'value'});
+        const parsedValue = cssTree.parse(decl.value, {context: 'value'});
 
-          cssTree.walk(parsedValue, {
-            visit: 'Identifier',
-            enter: function (node) {
-              if (node.name === initialColor.keyword) {
-                node.name = finalColor.keyword ? finalColor.keyword : finalColor.hex;
+        cssTree.walk(parsedValue, {
+          visit: 'Identifier',
+          enter: function (node) {
+            if (node.name === initialColor.keyword) {
+              node.name = finalColor.keyword ? finalColor.keyword : finalColor.hex;
+            }
+          }
+        });
+
+        cssTree.walk(parsedValue, {
+          visit: 'HexColor',
+          enter(node) {
+            if (node.value.length > 3) {
+              if (node.value === removeHashSymbol(initialColor.hex)) {
+                node.value = removeHashSymbol(finalColor.hex);
+              }
+            } else {
+              if (options.to.length > 3) {
+                node.value = removeHashSymbol(finalColor.hex);
+              } else if (node.value === convertToShortHex(removeHashSymbol(initialColor.hex))) {
+                node.value = convertToShortHex(removeHashSymbol(finalColor.hex));
               }
             }
-          });
+          }
+        });
 
-          cssTree.walk(parsedValue, {
-            visit: 'HexColor',
-            enter(node) {
-              if (node.value.length > 3) {
-                if (node.value === removeHashSymbol(initialColor.hex)) {
-                  node.value = removeHashSymbol(finalColor.hex);
-                }
-              } else {
-                if (options.to.length > 3) {
-                  node.value = removeHashSymbol(finalColor.hex);
-                } else if (node.value === convertToShortHex(removeHashSymbol(initialColor.hex))) {
-                  node.value = convertToShortHex(removeHashSymbol(finalColor.hex));
-                }
-              }
-            }
-          });
+        cssTree.walk(parsedValue, {
+          visit: 'Function',
+          enter(node) {
+            applyColorFunction(node, initialColor, finalColor);
+          }
+        });
 
-          cssTree.walk(parsedValue, {
-            visit: 'Function',
-            enter(node) {
-              applyColorFunction(node, initialColor, finalColor);
-            }
-          });
-
-          decl.value = cssTree.generate(parsedValue);
-        }
+        decl.value = cssTree.generate(parsedValue);
       });
     });
   };
