34323529a46a407490447770341fb295b4619df1
Anton Chernov <a.chernov@zaycev.net>
2018-04-12 14:40:31 +0500

    refactoring alpha channel checking

--- test/alfa-channel.spec.js
+++ test/alfa-channel.spec.js
@@ -5,7 +5,7 @@ describe('Alter Color plugin', () => {
 
   test('should alter simple color alpha channel in a single property', () => {
     return postcss()
-      .use(alterColorPlugin({from: 'black', to: 'red', alphaChannel: true}))
+      .use(alterColorPlugin({from: 'black', to: 'red', preserveAlphaChannel: true}))
       .process(`div{color:rgba(0,0,0,0.5)}`, {from: undefined})
       .then(result => {
         expect(result.css).toBe(`div{color:rgba(255,0,0,0.5)}`)
@@ -23,7 +23,7 @@ describe('Alter Color plugin', () => {
 
   test('should alter simple color alpha channel in a single property', () => {
     return postcss()
-      .use(alterColorPlugin({from: 'black', to: 'red', alphaChannel: true}))
+      .use(alterColorPlugin({from: 'black', to: 'red', preserveAlphaChannel: true}))
       .process(`div{color:rgba(0,0,0,0.5);outline:1px solid hsla(0,0%,0%,0.9);border:solid 1px rgb(0,0,0)}`, {from: undefined})
       .then(result => {
         expect(result.css).toBe(`div{color:rgba(255,0,0,0.5);outline:1px solid hsla(0,100%,50%,0.9);border:solid 1px rgb(255,0,0)}`)
