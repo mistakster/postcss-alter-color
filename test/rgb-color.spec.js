d97561e039b63b92cc2cd3d0cf4583f24fd56c15
Vladimir Kuznetsov <mistakster@gmail.com>
2018-04-20 09:19:45 +0500

    updated tests for rgb and rgba colors

--- test/rgb-color.spec.js
+++ test/rgb-color.spec.js
@@ -1,20 +1,25 @@
-const postcss = require('postcss');
-const alterColorPlugin = require('../lib/index');
+const process = require('./utils/process');
 
 it('should alter rgb colors', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:rgb(0,0,0)}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:rgb(255,0,0)}`)
-    });
+  const source = `
+div {
+  color: rgb(0,0,0);
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter rgb color in a complex value', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:rgb(0,0,0);border:1px solid rgb(255,255,255);outline:1px solid rgb(0,0,0)}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:rgb(255,0,0);border:1px solid rgb(255,255,255);outline:1px solid rgb(255,0,0)}`)
-    });
+  const source = `
+div {
+  color: rgb(0,0,0);
+  border: 1px solid rgb(255,255,255);
+  outline: 1px solid rgb(0,0,0);
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
--- test/rgba-color.spec.js
+++ test/rgba-color.spec.js
@@ -1,29 +1,38 @@
-const postcss = require('postcss');
-const alterColorPlugin = require('../lib/index');
+const process = require('./utils/process');
 
 it('should alter rgba colors', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:rgba(0,0,0,1)}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:rgba(255,0,0,1)}`)
-    });
+  const source = `
+div {
+  color: rgba(0,0,0,1);
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter rgba color in a complex value', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:rgba(0,0,0,1);border:1px solid rgba(255,255,255,1);outline:1px solid rgba(0,0,0,1)}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:rgba(255,0,0,1);border:1px solid rgba(255,255,255,1);outline:1px solid rgba(255,0,0,1)}`)
-    });
+  const source = `
+div {
+  color: rgba(0,0,0,1);
+  border: 1px solid rgba(255,255,255,1);
+  outline: 1px solid rgba(0,0,0,1);
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter rgba color and opacity in a complex value', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:rgba(0,0,0,0.5);border:1px solid rgba(255,255,255,1);outline:1px solid rgba(0,0,0,1)}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:rgba(0,0,0,0.5);border:1px solid rgba(255,255,255,1);outline:1px solid rgba(255,0,0,1)}`)
-    });
+  const source = `
+div {
+  color: rgba(0,0,0,0.5);
+  border: 1px solid rgba(255,255,255,1);
+  outline: 1px solid rgba(0,0,0,1);
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
