c696b5f6621e9b66ac0b77c909e5d5c7590b235d
Vladimir Kuznetsov <mistakster@gmail.com>
2018-04-20 09:09:21 +0500

    updated tests for hex colors

--- test/hex-color.spec.js
+++ test/hex-color.spec.js
@@ -1,47 +1,59 @@
-const postcss = require('postcss');
-const alterColorPlugin = require('../lib/index');
+const process = require('./utils/process');
 
 it('should alter short hex colors', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:#000}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:#f00}`)
-    });
+  const source = `
+div {
+  color: #000;
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter short hex to full hex colors', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: '#ff3fe0'}))
-    .process(`div{color:#000}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:#ff3fe0}`)
-    });
+  const source = `
+div {
+  color: #000;
+}
+`;
+
+  return process(source, {from: 'black', to: '#ff3fe0'})
+    .andMatchSnapshot();
 });
 
 it('should alter short hex color in a complex value', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:#000;border:1px solid #fff;outline:1px solid #000}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:#f00;border:1px solid #fff;outline:1px solid #f00}`)
-    });
+  const source = `
+div {
+  color: #000;
+  border: 1px solid #fff;
+  outline: 1px solid #000
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter full hex colors', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:#000000}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:#ff0000}`)
-    });
+  const source = `
+div {
+  color: #000000;
+}
+`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
 
 it('should alter full hex color in a complex value', () => {
-  return postcss()
-    .use(alterColorPlugin({from: 'black', to: 'red'}))
-    .process(`div{color:#000000;border:1px solid #ffffff;outline:1px solid #000000}`, {from: undefined})
-    .then(result => {
-      expect(result.css).toBe(`div{color:#ff0000;border:1px solid #ffffff;outline:1px solid #ff0000}`)
-    });
+  const source = `
+div {
+  color: #000000;
+  border: 1px solid #ffffff;
+  outline: 1px solid #000000;
+}`;
+
+  return process(source, {from: 'black', to: 'red'})
+    .andMatchSnapshot();
 });
